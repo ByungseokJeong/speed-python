fcd2498d470917d286a5c74b1600499bb7d22348
ByungseokJeong <newfuture@gmail.com>
2023-04-17 22:44:50 +0900

    패킹과 딕셔너리

---
+++ day4.py
@@ -0,0 +1,10 @@
+clovers = '클로버1', '클로버2', '클로버3'
+print(clovers)
+alice_blue=(240, 248, 255)
+r, g, b = alice_blue 
+print('R:', r, 'G:', g, 'B:', b) # 언패킹은 list도 가
+
+clover = {'나이':27, '직업': '병사'}
+clover['번호'] = 9 # 딕셔너리에 키 값 추
+print(clover)
+
