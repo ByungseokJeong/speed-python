0f28815efb4e4a2ec7eed7401a9d23df87beac33
ByungseokJeong <newfuture0401@gmail.com>
2023-04-25 20:58:26 +0900

    리드미 파일 추가

---
+++ day5.py
@@ -0,0 +1,24 @@
+a = 3
+b = 4
+print(a**b)
+
+a = 'python'
+b = 'python'
+print(a + b)
+
+print(a[0]) # 인덱싱
+print(a[::-1]) # 슬라이싱 
+
+a = "I eat %d apples. " % 3
+print(a)
+
+a = "%0.4f" % 3.4214321423
+
+print(a)
+
+a = "hobby"
+
+print(a.find ('b'))
+
+a = ","
+a.join('abcd')
